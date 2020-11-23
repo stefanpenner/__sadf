a66f324d7dc2c651f405ad1b35a370dc615614cb
Stefan Penner <stefan.penner@gmail.com>
2020-11-23 14:54:34 -0700

    Initial Commit

---
+++ Shared/sadfApp.swift
@@ -0,0 +1,17 @@
+//
+//  sadfApp.swift
+//  Shared
+//
+//  Created by Stefan Penner on 11/23/20.
+//
+
+import SwiftUI
+
+@main
+struct sadfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
